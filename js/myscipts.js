0fe3042502615fc7789c1382523e3722ada05a23
Shashank Kumar <63181812+shashanksanyasi@users.noreply.github.com>
2021-02-22 21:10:27 +0530

    Update myscipts.js

--- js/myscipts.js
+++ js/myscipts.js
@@ -1,78 +1,174 @@
- //this function will validate the data of the user....
-  function validateForm() {
-          
-       var fname      = document.forms["myForm"]["fname"].value;
-       var email     = document.forms["myForm"]["email"].value;
-       var phone     = document.forms["myForm"]["phone"].value;
-       var linkedin  = document.forms["myForm"]["linkedin"].value;
-       var date      = document.forms["myForm"]["date"].value;
-       var country   = document.forms["myForm"]["country"].value;
-       var resume    = document.forms["myForm"]["resume"].value;
-       var captcha   = document.forms["myForm"]["captcha"].value;
+$(function() {
 
-          if (fname == "" ) {
-                    
-           		document.getElementById("fname").style.border = '1px solid red';
-           		document.getElementById("fname_error").innerHTML  = "Please enter name!!";
-                return false;
-              }
-              else{
-              	document.getElementById("fname").style.border = '1px solid green';
-           		document.getElementById("fname_error").innerHTML  = "";
-              }
-          	if (email == "" ) {
-                    
-           		document.getElementById("email").style.border = '1px solid red';
-           		document.getElementById("email_error").innerHTML  = "Please enter email!!";
-                return false;
-              }
-              else{
-              	document.getElementById("email").style.border = '1px solid green';
-           		document.getElementById("email_error").innerHTML  = "";
-              }
-              if (phone == "" ) {
-                    
-           		document.getElementById("phone").style.border = '1px solid red';
-           		document.getElementById("phone_error").innerHTML  = "Please enter Phone!!";
-                return false;
-              }
-              else{
-              	document.getElementById("phone").style.border = '1px solid green';
-           		document.getElementById("phone_error").innerHTML  = "";
-              }
-               if (linkedin == "" ) {
-                    
-           		document.getElementById("linkedin").style.border = '1px solid red';
-           		document.getElementById("linkedin_error").innerHTML  = "Please enter linkedin!!";
-                return false;
-              }
-              else{
-              	document.getElementById("linkedin").style.border = '1px solid green';
-           		document.getElementById("linkedin_error").innerHTML  = "";
-              }
+	$("#fname_error").hide();
+	$("#email_error").hide();
+	$("#phone_error").hide();
+	$("#linkedin_error").hide();
+	$("#uploadField_error").hide();
+	$("#captcha_error").hide();
+	
 
-          if (country == "Select Country" ) {
-                    
-           		document.getElementById("country").style.border = '1px solid red';
-           		document.getElementById("country_error").innerHTML  = "please select country!!";
-                return false;
-              }
-              else{
-              	document.getElementById("country").style.border = '1px solid green';
-           		document.getElementById("country_error").innerHTML  = "";
-              }
-          	if (captcha == "" || captcha!="gGphJ") {
-                    
-           		document.getElementById("captcha").style.border = '1px solid red';
-           		document.getElementById("captcha_error").innerHTML  = "Please enter right captcha!!";
-                return false;
-              }
-              else{
-              	document.getElementById("captcha").style.border = '1px solid green';
-           		document.getElementById("captcha_error").innerHTML  = "";
-              }
-     		
-        }
+	var error_fname = false;
+	var error_email = false;
+	var error_phone = false;
+	var error_linkedin = false;
+	var error_uploadField = false;
+	var error_captcha = false;
 
-   	  
+	$("#fname").focusout(function() {
 
+		check_fname();
+		
+	});
+
+	$("#email").focusout(function() {
+
+		check_email();
+		
+	});
+
+	$("#phone").focusout(function() {
+
+		check_phone();
+		
+	});
+
+	$("#linkedin").focusout(function() {
+
+		check_linkedin();
+		
+	});
+	$("#uploadField").focusout(function() {
+
+		check_uploadField();
+		
+	});
+	
+	$("#captcha").focusout(function() {
+
+		check_captcha();
+		
+	});
+
+	function check_fname() {
+	
+		var fname_length = $("#fname").val().length;
+		
+		if(fname_length < 5 || fname_length > 20) {
+			$("#fname_error").html("Should be between 5-20 characters");
+			$("#fname_error").show();
+			error_fname = true;
+		} else {
+			$("#fname_error").hide();
+		}
+	
+	}
+	function check_email() {
+
+		var pattern = new RegExp(/^[+a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i);
+	
+		if(pattern.test($("#email").val())) {
+			$("#email_error").hide();
+		} else {
+			$("#email_error").html("Invalid email address");
+			$("#email_error").show();
+			error_email = true;
+		}
+	
+	}
+	function check_phone() {
+
+		var pattern = new RegExp(/^(\+91[\-\s]?)?[0]?(91)?[789]\d{9}$/i);
+	
+		if(pattern.test($("#phone").val())) {
+			$("#phone_error").hide();
+		} else {
+			$("#phone_error").html("Invalid phone no.");
+			$("#phone_error").show();
+			error_email = true;
+		}
+	
+	}
+	function check_linkedin() {
+	
+		var linkedin_length = $("#linkedin").val().length;
+		
+		if(linkedin_length < 10 || linkedin_length > 50) {
+			$("#linkedin_error").html("Should be between 10-50 characters");
+			$("#linkedin_error").show();
+			error_linkedin = true;
+		} else {
+			$("#linkedin_error").hide();
+		}
+	
+	}
+	function check_uploadField() {
+	
+		
+		var upload_length = $("#uploadField").val().length;
+		
+		if(upload_length < 1) {
+			$("#uploadField_error").html("Please insert the resume");
+			$("#uploadField_error").show();
+			error_uploadField = true;
+		} else {
+			$("#uploadField_error").hide();
+		}
+	
+	}
+
+	function check_captcha() {
+	
+		var captcha_length = $("#captcha").val();
+		
+		if(captcha_length != "gGphJ") {
+			$("#captcha_error").html("please re-enter the captcha");
+			$("#captcha_error").show();
+			error_captcha = true;
+		} else {
+			$("#captcha_error").hide();
+		}
+	
+	}
+
+
+
+	$("#myForm").submit(function() {
+											
+		error_fname = false;
+		error_email = false;
+		error_phone = false;
+		error_linkedin = false;
+		error_uploadField = false;
+		error_captcha = false;
+		
+											
+		check_fname();
+		check_email();
+		check_phone();
+		check_linkedin();
+		check_uploadField();
+		check_captcha();
+		
+		if(error_fname == false && error_email == false && error_phone == false && error_linkedin == false  && error_uploadField == false && error_captcha == false) {
+			return true;
+		} else {
+			return false;	
+		}
+
+	});
+
+});
+
+//date piceker function
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.datepicker').datepicker();
+  });
